d3629b2d6f8b1648e7bacfa0363924c280775768
iftikharkhan90 <iffirana708@gmail.com>
2025-03-30 05:26:41 +0500

    commit

--- server.js
+++ server.js
@@ -9,30 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.MONGO_DB || "mongodb://localhost:27017/mydatabase"; 
 // const allowedOrigins = [
-//     "http://localhost:5173",
-//     "*",
-//     "https://construction-management-site.netlify.app/"
-// ];
-
-// console.log("hi");
-
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             console.log("error");
-            
-//             callback(new Error("Not allowed by CORS"));
-//         }
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true
-// }));
-const cors = require("cors");
-const express = require("express");
-const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
